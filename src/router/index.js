497c6df91ca8b3df59a80f1c72f79912fb588117
Ézéchiel-kouakou <kouakouezechielk06@gmail.com>
2026-04-26 08:04:06 +0200

    nouvelle configuration - 26/04

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectsLibraryView from '../views/ProjectsLibraryView.vue'
 import ContactView from '../views/Contact.vue'  
@@ -64,7 +64,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
