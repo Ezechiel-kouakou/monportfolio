64210d69bf7407c02a5270ea19d24893693d3bd9
Ézéchiel-kouakou <kouakouezechielk06@gmail.com>
2026-05-01 14:41:30 +0200

    résolution de bug 01/05/26  14:33

--- public/video_proxy.php
+++ public/video_proxy.php
@@ -1,22 +1,19 @@
 <?php
 $file = $_GET['file'] ?? '';
+$source_url = "http://penguin.tailc4a1d9.ts.net/" . $file; // Test en HTTP
 
-if (empty($file) || !preg_match('/^video_[a-zA-Z0-9._-]+$/', $file)) {
-    http_response_code(400);
-    die("Fichier non valide.");
+// On essaie d'ouvrir le fichier
+$stream = @fopen($source_url, 'rb');
+
+if (!$stream) {
+    // Si ça échoue, on affiche l'erreur système pour comprendre pourquoi
+    $last_error = error_get_last();
+    header("Content-Type: text/plain");
+    echo "ERREUR DE CONNEXION : \n";
+    echo "Cible : " . $source_url . "\n";
+    echo "Message : " . $last_error['message'];
+    exit;
 }
-
-$penguin_url = "https://penguin.tailc4a1d9.ts.net/" . $file;
-
 
 header('Content-Type: video/mp4');
-header('Access-Control-Allow-Origin: *');
-
-$stream = fopen($penguin_url, 'rb');
-if ($stream) {
-    fpassthru($stream);
-    fclose($stream);
-} else {
-    http_response_code(404);
-    echo "Vidéo introuvable sur le serveur source.";
-}
+fpassthru($stream);
